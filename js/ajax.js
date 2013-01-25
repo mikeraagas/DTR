c324877002c8fbf535a89a5f1db1296c9bb9b6f4
Michael Raagas <michael.dpoint@gmail.com>
2013-01-25 17:22:46 +0800

    delete jquer-ui folder

---
+++ js/ajax.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function()) {
+	// if submit button is clicked
+	$('#submit').click(function()) {
+
+		// Get data from all the fields 
+		var employee = $('input[name=employee]')
+		var start_date = $('input[name=start-date]');
+		var end_date = $('input[name=end-date]');
+
+	}
+}
