19c62b7c4dea3fc82f34ca4369f5405988125d35
Michael Raagas <michael.dpoint@gmail.com>
2013-01-25 17:31:38 +0800

    ajax uncomplete

--- js/ajax.js
+++ js/ajax.js
@@ -8,5 +8,16 @@ $(document).ready(function()) {
 		var start_date = $('input[name=start-date]');
 		var end_date = $('input[name=end-date]');
 
+		// Simple validation to make sure user entered something
+		// If error found, add highlighting class to the text field
+		if (employee.val() == '') {
+			employee.addClass('higlight');
+			return false;
+		} else name.removeClass('highlight');
+
+		if (start_date.val() == '') {
+			start_date.
+		}
+
 	}
 }
